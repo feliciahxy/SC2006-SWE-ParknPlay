8bc3aa0d1aa78f587cabaa4e5b02e83f4c1d4053
ChewJinCheng <jinjc2104@gmail.com>
2024-11-03 20:13:21 +0800

    Email validator for register

--- backend/api/views/userMgr.py
+++ backend/api/views/userMgr.py
@@ -13,8 +13,11 @@ class CreateUserView(generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         username = request.data.get('username')
+        email = request.data.get('email')
         if User.objects.filter(username=username).exists():
             return Response({"detail": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
+        if User.objects.filter(email=email).exists():
+            return Response({"detail": "Email already registered"}, status=status.HTTP_400_BAD_REQUEST)
         return super().create(request, *args, **kwargs)
 
 def home(request):
